cd8106386c4c04608b9e5af70edf358797044445
Taboichebet <taboibrenda@gmail.com>
2022-08-28 15:45:57 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = word.split("").reverse().join("");
+  if(word === reversedWord){
+    return true;
+  } else {
+    return false;
 }
-
+}
 /* 
   Add your pseudocode here
 */
